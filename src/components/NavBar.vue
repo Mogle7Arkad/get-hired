9c2e0ec7aa21b674373ff36711c078fe38724677
Ali George <george7ali@gmail.com>
2024-10-06 13:04:43 +0100

    edit class in the navbar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,11 +3,11 @@
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="flex h-20 items-center justify-between">
         <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-          <a class="flex flex-shrink-0 items-center mr-4 mb-2" href="/">
+          <a class="flex flex-shrink-0 items-center mr-4" href="/">
             <img class="h-16 w-auto" :src="logo" alt="get hired" />
             <span class="hidden md:block text-white text-2xl font-bold ml-2">Bluelily Suites</span>
           </a>
-          <div class="md:ml-auto">
+          <div class="md:ml-auto mt-2">
             <div class="flex space-x-2">
               <a
 
