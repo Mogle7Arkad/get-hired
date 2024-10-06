7c49b061f802fc929848a3a5fa366ed842546285
Ali George <george7ali@gmail.com>
2024-10-06 09:08:58 +0100

    change the font family from the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Poppins', 'sans serif']
-      },
+
       gridTemplateColumns: {
         '70/30': '70% 28%'
       }
