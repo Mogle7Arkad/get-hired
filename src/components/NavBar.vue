bfb6b4b3d48493ce5bce90ea111a582b156b062a
Ali George <george7ali@gmail.com>
2024-10-06 09:25:00 +0100

    adjust tailwind class for the logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,8 +3,8 @@
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="flex h-20 items-center justify-between">
         <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-          <a class="flex flex-shrink-0 items-center mr-4" href="/">
-            <img class="h-10 w-auto" :src="logo" alt="get hired" />
+          <a class="flex flex-shrink-0 items-center mr-4 mb-2" href="/">
+            <img class="h-16 w-auto" :src="logo" alt="get hired" />
             <span class="hidden md:block text-white text-2xl font-bold ml-2">Bluelily Suites</span>
           </a>
           <div class="md:ml-auto">
