c87239cc7052c35c90cba629ed77d0a06ad7c10b
Ali George <george7ali@gmail.com>
2024-10-08 13:15:45 +0100

    created the homecard component and exported it to the app component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <NavBar />
   <HeroSec />
+  <HomeCards />
 </template>
 
 <script setup>
 import NavBar from "./components/NavBar.vue";
 import HeroSec from "./components/HeroSec.vue";
+import HomeCards from "./components/HomeCards.vue";
 </script>
