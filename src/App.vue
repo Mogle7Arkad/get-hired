e06205c86721046a6208174f1831e46cc3063db0
Ali George <george7ali@gmail.com>
2024-10-17 00:44:48 +0100

    create new component and import in the App component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <NavBar />
   <HeroSec />
   <HomeCards />
+  <JobsListings />
 </template>
 
 <script setup>
 import NavBar from "./components/NavBar.vue";
 import HeroSec from "./components/HeroSec.vue";
 import HomeCards from "./components/HomeCards.vue";
+import JobsListings from "./components/JobsListings.vue";
 </script>
