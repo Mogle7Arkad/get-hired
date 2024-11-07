8f256fbdc5550903bb7c8033747297363a80838b
Ali George <george7ali@gmail.com>
2024-11-07 23:32:11 +0100

    import defineprops to enable props to be use for the title and subtitle of the hero section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,5 +1,16 @@
 <script setup>
+import { defineProps } from 'vue';
 
+defineProps({
+    title: {
+        type: String,
+        default: 'Get employed'
+    },
+    subtitle: {
+        type: String,
+        default: 'Find the right job that fits your skills and needs'
+    },
+});
 </script>
 <template>
     <section class="bg-green-700 py-20 mb-4">
