b5037c29df039cb8ef58a79d4affe5905a786ac8
Ali George <george7ali@gmail.com>
2024-11-07 22:40:23 +0100

    change name

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,7 +11,7 @@ import logo from '../assets/img/logo.png';
             <a class="flex flex-shrink-0 items-center mr-4" href="index.html">
               <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
               <span class="hidden md:block text-white text-2xl font-bold ml-2"
-                >Vue Jobs</span
+                >Naijagigs</span
               >
             </a>
             <div class="md:ml-auto">
