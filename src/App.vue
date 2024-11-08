5b822cfdfcbbb2a2873369861b5764e3cca18101
Ali George <george7ali@gmail.com>
2024-11-08 08:31:29 +0100

    import the homecard component into app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import HeroSection from './components/HeroSection.vue';
+import HomeCard from './components/HomeCard.vue';
 </script>
 
 <template>
   <NavBar />
   <HeroSection />
+  <HomeCard />
 </template>
