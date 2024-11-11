f65ec1ca487616342ef1547a2d5bc95556e2d4bd
Ali George <george7ali@gmail.com>
2024-11-12 00:15:36 +0100

    import jobdata and ref, and loop through jobs to display job titles for a test by v binding

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,6 +1,11 @@
 <script setup>
+import jobData from './jobs.json';
+import { ref } from 'vue';
 
+const jobs = ref(jobData);
 </script>
 <template>
-    Job Listings
+    <div v-for="job in jobs" :key="job.id">
+        {{ job.title }}
+    </div>
 </template>
