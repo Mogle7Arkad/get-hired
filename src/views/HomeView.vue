08ba9303d4f38867caccac750f877b9843da92b7
Ali George <george7ali@gmail.com>
2024-11-12 14:19:41 +0100

    moving components to homeview

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeroSection from './components/HeroSection.vue';
+import HomeCard from './components/HomeCard.vue';
+import JobListings from './components/JobListings.vue';
+</script>
+
+<template>
+    <HeroSection />
+    <HomeCard />
+    <JobListings />
+</template>
