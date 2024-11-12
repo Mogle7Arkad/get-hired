22de69d17af6d3685ad53fa06bfa75f6233e8430
Ali George <george7ali@gmail.com>
2024-11-12 11:41:09 +0100

    import primeicons into the main.js file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import 'primeicons/primeicons.css';
+
 import './assets/main.css';
-import App from './App.vue'
+import App from './App.vue';
 
 createApp(App).mount('#app')
