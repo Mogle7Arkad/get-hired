97e393be526d4aad58b0cc7fa7dea26978731d4c
Ali George <george7ali@gmail.com>
2024-11-15 10:22:17 +0100

    implement vue router and import it to the app for display

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import 'primeicons/primeicons.css';
+import router from './router';
 
 import './assets/main.css';
 import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
-import HeroSection from './components/HeroSection.vue';
-import HomeCard from './components/HomeCard.vue';
-import JobListings from './components/JobListings.vue';
+import HeroSection from '../components/HeroSection.vue';
+import HomeCard from '../components/HomeCard.vue';
+import JobListings from '../components/JobListings.vue';
 </script>
 
 <template>
